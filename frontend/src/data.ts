db8cb20d1da5b640fd964d4e3992eb24fedb4263
Nick Lucas <me@nicklucas.co.uk>
2025-09-26 14:19:55 +0100

    Reorganise

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -14,12 +14,12 @@ export interface SamplerResult {
   tree: TreeNode;
 }
 
-import ingredientForSoup from "../../outputs/token-tree--a-great-ingredient-to-put-in-soup-is.json";
 import capitalOfFrance from "../../outputs/token-tree--the-capital-of-france-is.json";
+import ingredientForSoup from "../../outputs/token-tree--a-great-ingredient-to-put-in-soup-is.json";
 import meaningOfLife from "../../outputs/token-tree--the-meaning-of-life-is.json";
 
 export const data: { title: string; result: SamplerResult }[] = [
-  { title: "A great ingredient to put in soup", result: ingredientForSoup },
   { title: "The capital of France", result: capitalOfFrance },
+  { title: "A great ingredient to put in soup", result: ingredientForSoup },
   { title: "The meaning of life", result: meaningOfLife },
 ];
